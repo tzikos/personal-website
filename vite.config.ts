d9475090e3b72f50e1a457560707e43a1faadf0c
tzikos <s242798@dtu.dk>
2025-03-31 16:46:29 +0200

    fixes port bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'  // Change this line
-import path from "path";
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+    host: true, // This is important for Render deployment
+  },
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+    host: true, // This is important for Render deployment
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  server: {
-    port: 8080
   }
-});
+})
